02de5e7042de659f2b7f6da2a002f7b03a8aacd6
jakub <kniemiec3@gmail.com>
2020-04-28 00:53:32 +0200

    reaction loop updated

--- tests/reactiontime_app.js
+++ tests/reactiontime_app.js
@@ -1,11 +1,12 @@
 // DOM elements
 const TEST = document.getElementById("test");
-const reactionTime = document.getElementById("reaction-time");
+const averageTime = document.getElementById("average-time");
 const triesNumber = document.getElementById("tries-number"); 
 const img = document.querySelector(".main-img");
 const dots = document.querySelector(".dots")
-const title = document.querySelector(".title");
+const title = document.getElementById("title");
 const subtitle = document.querySelectorAll(".group-test h2");
+
 
 
 const randomDelay = (minimum, maximum) => {
@@ -18,11 +19,11 @@ const init = () => {
     // changes bg to red and awaits green
     console.log('initment')
     TEST.style.backgroundColor = "rgba(163, 36, 20,0.8)";
-    img.style.display = "none"
-    dots.style.display = "block"
-    title.textContent = "Wait for green"
+    img.style.display = "none";
+    dots.style.display = "block";
+    title.textContent = "Wait for green";
     subtitle.forEach(element => {
-        element.style.display = "none"
+        element.style.display = "none";
     });
 }
 
@@ -33,44 +34,64 @@ const changeForGreen = async (delay) => {
         setTimeout(() => {
             TEST.style.backgroundColor = "rgba(60, 230, 90,0.75)";
             title.textContent = "Click!";
-            resolve("true")
-        }, delay)
+            resolve("true");
+        }, delay);
     })  
 }
-
 
 
 const react = async () => {
     return new Promise((resolve, reject) => {
         TEST.addEventListener("click", async () => {
-            end = performance.now()
+            end = performance.now();
             resolve(end);
         })
     })
 }
 
 
+const showResult = (time, iteration) => {
+    console.log("showing results");
+    TEST.style.backgroundColor = "rgb(43, 135, 209)";
+    title.textContent = `${time} ms`;
+    triesNumber.textContent = `${iteration+1} of 5`
+    averageTime.textContent = `${time}ms`;
+    subtitle[0].textContent = "Click to keep going";
+    subtitle[0].style.display = "block";
+}
 
-// TEST.addEventListener("click", async () => {
-//     const end = await react()
-// })
+
+const waitForUser = async () => {
+    return new Promise((resolve, reject) => {
+        TEST.addEventListener("click", async () => {
+            resolve(end);
+        });
+    });
+}
 
 
 const test = async () => {
     console.log("Test started");
+    const resutlArray = []
     const reactionTime = document.getElementById("reaction-time");
     const triesNumber = document.getElementById("tries-number");
     const delay = randomDelay(1000,8000);
 
     TEST.removeEventListener("click", test);
 
-    init();
-    const wait = await changeForGreen(delay);
-    if (wait) {
-        let start = performance.now()   //starts timing
-        const end = await react()       // ends timing and returns performance.now()
-        const time = end - start        // calculates time
-    }
+    for(let i=0; i<5; i++){
+        init()
+        const wait = await changeForGreen(delay);
+        if (wait) {
+            let start = performance.now();                  //starts timing
+            const end = await react();                      // ends timing and returns performance.now()
+            const time = Math.floor(end - start);           // calculates time
+            resutlArray.push(time)
+            showResult(time,i);
+            await waitForUser()                                // waits for user to click in order to move
+        };
+    };
+    console.log("end");
 }
 
 
